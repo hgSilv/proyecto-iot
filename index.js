dce0b238cffcc9c1cd347a14d4813e005fe5b1bb
Paulina Arvizu <is707571@iteso.mx>
2022-04-30 20:47:43 -0500

    activate servo if dog detected

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
 const { spawn } = require('child_process');
 
-function run(name, command, args) {
+function runMQTT() {
+    const process = spawn('mosquitto_sub', ['-h', 'broker.mqttdashboard.com', '-p', '1883', '-u', 'pollo', '-P', 'pollo', '-t', 'dogtrainer', '-i', 'linuxclient']);
+    // collect data from script
+    process.stdout.on('data', function (data) {
+        let message = data.toString().trim();
+        if (message == 'turn on') {
+            console.log('start edge-impulse');
+            run_edge();
+        }
+        else
+            console.log('no treat');
+    });
+    // in close event we are sure that stream from child process is closed
+    process.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code} (MQTT})`);
+    });
+}
+
+function run_edge() {
+    const process = spawn('node', ['/lib/node_modules/edge-impulse-linux/build/cli/linux/runner.js']);
+    // collect data from script
+    process.stdout.on('data', function (data) {
+        let output = data.toString().trim();
+        if (output == 'Dog detected, activating servo') {
+            console.log(output);
+            run_script('servo', 'python', ['servopi.py']);
+        }
+    });
+    // in close event we are sure that stream from child process is closed
+    process.on('close', (code) => {
+        console.log(`child process close all stdio with code ${code} (runner)`);
+    });
+}
+
+function run_script(name, command, args) {
     const process = spawn(command, args);
     // collect data from script
     process.stdout.on('data', function (data) {
         // console.log('Running runner.js ...');
         console.log(data.toString());
-    });
-    // in close event we are sure that stream from child process is closed
-    process.on('close', (code) => {
-        console.log(`child process close all stdio with code ${code} (${name})`);
-    });
-}
-
-function runMQTT() {
-    const process = spawn('mosquitto_sub', ['-h', 'broker.mqttdashboard.com', '-p', '1883', '-u', 'pollo', '-P', 'pollo', '-t', 'dogtrainer', '-i', 'linuxclient']);
-    // collect data from script
-    process.stdout.on('data', function (data) {
-        var message = data.toString().trim();
-        if (message == 'release treat') {
-            console.log('start edge-impulse');
-            run("runner", 'node', ['/lib/node_modules/edge-impulse-linux/build/cli/linux/runner.js'])
-        }
-        else
-            console.log('no treat');
     });
     // in close event we are sure that stream from child process is closed
     process.on('close', (code) => {
